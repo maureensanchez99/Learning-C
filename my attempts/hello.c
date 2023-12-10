37f8c2abb9417e3cc22fccfedb442d62fb7c68b8
Maureen Sanchez <39846411+maureensanchez99@users.noreply.github.com>
2023-12-10 17:44:16 -0600

    editing hello.c

--- my attempts/hello.c
+++ my attempts/hello.c
@@ -2,7 +2,12 @@
 #include <string.h>
 #include <stdlib.h>
 
+void printBanner(){
+    printf("----------\n");
+}
+
 int main(int argc, char *argv[]){
+    printBanner();
     printf("Hello.\n");
     
     return 0;
