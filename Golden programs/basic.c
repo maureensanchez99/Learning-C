1787a1f9db7922e1366d0c1f54ed61402bd61e46
Maureen Sanchez <39846411+maureensanchez99@users.noreply.github.com>
2023-12-12 15:05:24 -0600

    working on basic.c

--- Golden programs/basic.c
+++ Golden programs/basic.c
@@ -10,5 +10,12 @@ void do_something(char a, int b, long c, long long d, float e, double f, unsigne
 
 void do_something(char a, int b, long c, long long d, float e, double f, unsigned int g, unsigned 
                   long h, unsigned long long i){
-    printf("a=%c, b=%d, c=%1d, d=");
+    printf("a=%c, b=%d, c=%1d, d=%11d, e=%f, f=%f, g=%u, h=%1u, i=%11u\n");
 } 
+
+int main(int argc, char *argv[]){
+    int a = 5;
+    int i;
+    
+    return 0;
+}
