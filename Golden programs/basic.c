0519148c2d1df8edbfd65fa1ec503575747d7e13
Maureen Sanchez <39846411+maureensanchez99@users.noreply.github.com>
2023-12-13 23:17:53 -0600

    adding in do and do while loops

--- Golden programs/basic.c
+++ Golden programs/basic.c
@@ -3,10 +3,10 @@
 #include <string.h>
 
 //function prototype -- helps the compiler do type checking of arguments and return values
-void do_something(char a, int b, long c, long long d, float e, double f, unsigned int g, unsigned 
+/*void do_something(char a, int b, long c, long long d, float e, double f, unsigned int g, unsigned 
                   long h, unsigned long long i){
     
-}
+}*/
 
 void do_something(char a, int b, long c, long long d, float e, double f, unsigned int g, unsigned 
                   long h, unsigned long long i){
@@ -17,5 +17,29 @@ int main(int argc, char *argv[]){
     int a = 5;
     int i;
     
+    do_something('a', -3, 4, 5, 5.6, 4.1415926535, 5, 99, 128);
+
+    // IF
+    if(a == 0){
+        printf("a=0\n");
+    } else if (a == 1){
+        printf("a=1\n");
+    } else {
+        printf("a is neither 0 nor 1.\n");
+    }
+
+    /* WHILE. The condtion (in this case, the integer variable 'a')
+       can be anything that evaluates to a numeric value. 0 is false, 
+       anything else is true. The loop terminates when the condtion
+       evaluates to false and is checked BEFORE each loop iteration
+    */
+   while(a){
+        a--;
+        printf("a=%d\n, a");
+   }
+   a = -4;
+
+   /* DO WHILE. The condtion 
+   */ 
     return 0;
 }
